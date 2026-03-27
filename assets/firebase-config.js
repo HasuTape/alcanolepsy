4474c0407e6b35f08ef558d211af1bdafc11cd63
HasuTape <wiktorwozny5555@gmail.com>
2026-03-27 22:59:44 +0100

    Add files via upload

---
+++ assets/firebase-config.js
@@ -0,0 +1,11 @@
+// UZUPELNIJ danymi z Firebase Console -> Project settings -> Your apps (Web app)
+// To jest celowo plik "z kodem" (bez builda), żeby strona mogła być hostowana jako statyczna.
+export const firebaseConfig = {
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
+};
+
